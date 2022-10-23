f7647d4632563c809c2fafad179964be5f1c03eb
codernoyan <coder.noyan@gmail.com>
2022-10-24 01:08:03 +0600

    places api created

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const places = require('./data/places.json');
+
+app.get('/', (req, res) => {
+  res.send('Travel Guru server is running')
+});
+
+app.get('/places', (req, res) => {
+  res.send(places);
+})
+
+app.get('/places/:name', (req, res) => {
+  const name = req.params.name;
+  const placeName = places.find(place => place.name === name);
+  res.send(placeName);
+})
+
+app.listen(port, () => {
+  console.log(`Travel Guru server is running on port: ${port}`)
+});
